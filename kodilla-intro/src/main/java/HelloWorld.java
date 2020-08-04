21f59256e21b262e56520070eed5cb2ccd277a81
Anna Walus <anush2604@gmail.com>
2020-08-04 22:12:10 +0200

    Zadanie 1

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from FirstClass!");
+        System.out.println("Ania");
+        System.out.println("30");
+        System.out.println("W");
+
+    }
+}
+
