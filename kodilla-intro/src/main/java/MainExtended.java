e4dde8e2c8ad5e5caf7b80a8025360a8c84855cf
Anna Walus <anush2604@gmail.com>
2020-08-04 22:54:02 +0200

    Zadanie zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String nickname = "Hanka";
+        String origin = "Z Polski";
+        int myModule = 0;
+        double piNumber = 3.14;
+        char bloodGroup = 'B';
+        boolean isASwimmer = false;
+        System.out.println(nickname);
+        System.out.println(myModule);
+        System.out.println(origin);
+        System.out.println(piNumber);
+        System.out.println(bloodGroup);
+        System.out.println(isASwimmer);
+    }
+}
