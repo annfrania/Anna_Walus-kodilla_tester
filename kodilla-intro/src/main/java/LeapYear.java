28f3efabf60b039a38b0ed4552d24678cbd7b65d
Anna Walus <anush2604@gmail.com>
2020-08-06 07:23:46 +0200

    LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,16 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        boolean result = 1990%4 = 0;
+        System.out.println(result);
+        if (result = 0){
+            System.out.println("Rok przestępny!");
+        }else {
+            System.out.println("Rok nieprzestępny!");
+        }
+        int result = 1990%4 = 0;
+        return result;
+
+    }
+}
+
+
