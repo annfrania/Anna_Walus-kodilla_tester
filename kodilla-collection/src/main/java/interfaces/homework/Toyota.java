d4d51b4bd49552f3bf7cfb085d63ed06597c33ba
Anna Walus <anush2604@gmail.com>
2020-10-09 00:57:37 +0200

    m.4.CarsList

--- kodilla-collection/src/main/java/interfaces/homework/Toyota.java
+++ kodilla-collection/src/main/java/interfaces/homework/Toyota.java
@@ -1,5 +1,7 @@
 package interfaces.homework;
 
+import java.util.Objects;
+
 public class Toyota implements Car {
     int speed;
 
@@ -20,5 +22,24 @@ public class Toyota implements Car {
     @Override
     public void decreaseSpeed() {
     speed = speed - 6;
+    }
+
+    @Override
+    public String toString() {
+        return "Toyota{" +
+                "speed=" + speed +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Toyota toyota = (Toyota) o;
+        return speed == toyota.speed;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(speed);
     }
 }
