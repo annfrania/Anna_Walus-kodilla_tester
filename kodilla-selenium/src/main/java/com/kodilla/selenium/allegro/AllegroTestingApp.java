7f6956e2ca74e995c593b06b604083d16737d943
Anna Walus <anush2604@gmail.com>
2021-03-04 00:27:39 +0100

    m.13 homework

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,34 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AllegroTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://allegro.pl/");
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        WebElement inputField = driver.findElement(By.xpath(
+                "*//form[starts-with(@class,\"mp7g_oh\")]/input")); // [4]
+        inputField.sendKeys("Mavic mini");
+
+        WebElement category = driver.findElement(
+                By.xpath("*//form[starts-with(@class,\"mp7g_oh\")]/div/div/select"));
+
+        Select categorySelection;
+        categorySelection = new Select(category);
+        categorySelection.selectByIndex(2);
+
+        WebElement search = driver.findElement(By.xpath("*//form[starts-with(@class,\"mp7g_oh\")]/button"));
+        search.click();
+    }
+
+}
