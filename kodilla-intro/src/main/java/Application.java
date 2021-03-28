f676ca30b1f82812b2ed658416550c4e434d041a
Anna Walus <anush2604@gmail.com>
2021-03-28 18:59:36 +0200

    m.2 zad.2.6 brakujące

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -9,11 +9,12 @@ public class Application {
         this.height = height;
     }
     public void checkAgeAndHeight() {
-        if (this.age > 30 && this.height > 160) {
-            System.out.println("User is older than 30 and higher then 160cm");
-        }
-        else {
-            System.out.println("User is younger than 30 or shorter  than 160cm");
+        if  (name != null) {
+            if (this.age > 30 && this.height > 160) {
+                System.out.println("User is older than 30 and higher then 160cm");
+            } else {
+                System.out.println("User is younger than 30 or shorter than 160cm");
+            }
         }
     }
 }
