f2c1d5da0e5dbb7829a25f31f99274d30e4ef5ee
Anna Walus <anush2604@gmail.com>
2022-01-06 23:03:07 +0100

    exc

---
+++ moje/src/main/java/CalcCucumber.java
@@ -0,0 +1,6 @@
+
+public class CalcCucumber {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ moje/src/main/resources/Sum.feature
@@ -0,0 +1,4 @@
+Scenario: Summing with zero
+Given I test calculator
+When I add 0 and 3
+Then result is 3
